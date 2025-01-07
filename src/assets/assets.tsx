d53eef4cc6b3df765a033f55ddb2288ff76f43a5
Tejasv <tejasv@Administrators-MacBook-Air.local>
2025-01-07 16:01:48 +0530

    add asset.tsx

---
+++ src/assets/assets.tsx
@@ -0,0 +1,25 @@
+import search_icon from '../assets/search.svg';
+import cloud_sunny from '../assets/cloud-sunny.svg';
+import cloud from '../assets/cloud.svg';
+import heavy_rain from '../assets/heavy-rain.svg';
+import rain from '../assets/rain.svg';
+import snow from '../assets/snow.svg';
+import sun_light from '../assets/sun-light.svg';
+import thunderstorm from '../assets/thunderstorm.svg';
+import humidity from '../assets/menu.svg';
+import wind from '../assets/wind.svg';
+import map_pin from '../assets/map-pin.svg';
+
+export {
+  search_icon,
+  cloud_sunny,
+  cloud,
+  heavy_rain,
+  rain,
+  snow,
+  sun_light,
+  thunderstorm,
+  humidity,
+  wind,
+  map_pin
+};
